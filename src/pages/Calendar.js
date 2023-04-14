c445bfe28272fb9bc36ed4d7f183d0b917da2360
tuankiet091002 <tuankiet091002@gmail.com>
2023-04-14 22:22:36 +0700

    a little better calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid'
 import { Container, Typography } from '@mui/material';
 
 import { useTaskContext } from '../hooks/Tasks/useTaskContext';
@@ -18,8 +19,8 @@ const Calendar = () => {
     const taskCalendar = tasks.map(task => {
         return {
             title: `Task ${task._id}: ${task.state}`,
-            start: (new Date(task.date).getTime()+3600000*(task.shift*3+3)),
-            end: (new Date(task.date).getTime()+3600000*(task.shift*3+6)),
+            start: (new Date(task.date).getTime() + 3600000 * (task.shift * 3 + 3)),
+            end: (new Date(task.date).getTime() + 3600000 * (task.shift * 3 + 6)),
             url: '/task'
         }
     })
@@ -28,11 +29,16 @@ const Calendar = () => {
 
     return (<Container maxWidth={false} sx={{ mx: 0, height: '100%', width: '100%' }}>
         <FullCalendar
-            plugins={[dayGridPlugin]}
-            initialView="dayGridMonth"
+            plugins={[dayGridPlugin, timeGridPlugin]}
+            initialView="timeGridWeek"
             height='100%'
             aspectRatio={1}
             events={taskCalendar}
+            headerToolbar={{
+                left: 'prev,next',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay' // user can switch between the two
+            }}
 
         /></Container>
     )
