175c8a7755b05fd91751c514f7ce26b0576f544c
tuankiet091002 <tuankiet091002@gmail.com>
2023-04-19 10:03:47 +0700

    map search function

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,245 +1,134 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { TextField, FormControl, InputLabel, MenuItem, Select, Button, Container, Typography } from '@mui/material';
+import { TextField, FormControl, InputLabel, MenuItem, Select, Button, Container, Typography, ButtonGroup } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
-import { Icons } from '../icon.js';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-
+import { useMap } from 'react-leaflet';
+import 'leaflet-routing-machine';
+import GetLocation from '../components/Map/MapLocation.js';
 import MapSearch from '../components/Map/MapSearch.js';
-
-var MCPfull = L.icon({
-    //iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',  
-    iconUrl: Icons.trashFullIcon,
-    iconSize: [38, 38],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-});
-var MCPemp = L.icon({
-    //iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',  
-    iconUrl: Icons.trashEmptyIcon,
-    iconSize: [38, 38],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-});
-var MCPhalf = L.icon({
-    //iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',  
-    iconUrl: Icons.trashHaflIcon,
-    iconSize: [38, 38],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-});
-var truckIcon = L.icon({
-    //iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',  
-    iconUrl: Icons.TruckIcon,
-    iconSize: [38, 38],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-});
-
-var mylocation = L.icon({
-    iconUrl: Icons.MyloIcon,
-    iconSize: [38, 38],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-});
-
-console.log(Icons);
-// function ComponentDidMount(){
-//   navigator.geolocation.getCurrentPosition((position) => {
-//     this.setState({
-//       latitude: position.coords.latitude, 
-//       longitude: position.coords.longitude
-//     })
-//   });
-// }
-
+import { truckIcon, MCPfull, MCPhalf, MCPemp } from '../components/Map/MapEntity.js';
+import DialogMCP from '../components/Map/DialogMCP.js';
+import { useMCPContext } from '../hooks/MCPs/useMCPContext.js';
+import { useGetMCPs } from '../hooks/MCPs/useGetMCPs.js';
+import { useGetTasks } from '../hooks/Tasks/useGetTasks.js';
+import { useDeleteMCP } from '../hooks/MCPs/useDeleteMCP.js';
+import { useGetTrucks } from '../hooks/Trucks/useGetTrucks.js';
+import DeleteIcon from '@mui/icons-material/Delete';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import { useTruckContext } from '../hooks/Trucks/useTruckContext.js';
+import './map.css';
+import Routing from '../components/Map/Routing.js';
 const Map = () => {
+    const { mcps } = useMCPContext();
+    const { trucks } = useTruckContext();
+    const { getMCPs } = useGetMCPs();
+    const { deleteMCP } = useDeleteMCP();
+    const { getTrucks } = useGetTrucks();
+    const { getTasks } = useGetTasks();
 
-    const [truckLocation, setTruckLocation] = useState({
-        location: {
-            lat: 108.6,
-            lng: 10
-        },
-        zoom: 2,
-        situation: 10
-    });
+    const [display, setDisplay] = useState(0);
+    const [displayMCP, setDisplayMCP] = useState(mcps);
+    const [displayTruck, setDisplayTruck] = useState(trucks);
+    const [open, setOpen] = useState(false);
+
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition((location) => {
-            const { latitude, longitude } = location.coords;
-            setTruckLocation({
-                location: {
-                    lat: latitude - 0.001,
-                    lng: longitude - 0.004
-                },
-                zoom: 13,
-                situation: 10
-            });
-        }, (error) => {
-            console.log(error);
-        });
+        getMCPs();
+        getTrucks();
+        getTasks();
     }, []);
 
-    const [MCPFullLocation, setMCPFullLocation] = useState({
-        location: {
-            lat: 108.6,
-            lng: 10
-        },
-        zoom: 2,
-        situation: 10
-    });
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition((location) => {
-            const { latitude, longitude } = location.coords;
-            setMCPFullLocation({
-                location: {
-                    lat: latitude - 0.003,
-                    lng: longitude + 0.01
-                },
-                zoom: 13,
-                situation: 10
-            });
-        }, (error) => {
-            console.log(error);
-        });
-    }, []);
-    const [MCPHalfLocation, setMCPHalfLocation] = useState({
-        location: {
-            lat: 108.6,
-            lng: 10
-        },
-        zoom: 2,
-        situation: 10
-    });
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition((location) => {
-            const { latitude, longitude } = location.coords;
-            setMCPHalfLocation({
-                location: {
-                    lat: latitude + 0.001,
-                    lng: longitude + 0.01
-                },
-                zoom: 13,
-                situation: 10
-            });
-        }, (error) => {
-            console.log(error);
-        });
-    }, []);
-    const [MCPEmpLocation, setMCPEmpLocation] = useState({
-        location: {
-            lat: 108.6,
-            lng: 10
-        },
-        zoom: 2,
-        situation: 10
-    });
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition((location) => {
-            const { latitude, longitude } = location.coords;
-            setMCPEmpLocation({
-                location: {
-                    lat: latitude + 0.001,
-                    lng: longitude + 0.001
-                },
-                zoom: 13,
-                situation: 10
-            });
-        }, (error) => {
-            console.log(error);
-        });
-    }, []);
+        if (display !== 1)
+            setDisplayMCP(mcps);
+        else setDisplayMCP([]);
 
-    const [position, setPosition] = useState({
-        location: {
-            lat: 0,
-            lng: 0
-        },
-        zoom: 2,
-        haveMCPLocation: false
-    });
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition((location) => {
-            const { latitude, longitude } = location.coords;
-            setPosition({
-                location: {
-                    lat: latitude,
-                    lng: longitude
-                },
-                zoom: 13,
-                haveMCPLocation: true
-            });
-        }, (error) => {
-            console.log(error);
-        });
-    }, []);
+    }, [mcps]);
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => {
-        setOpen(true);
+    useEffect(() => {
+        if (display !== 2)
+            setDisplayTruck(trucks);
+        else setDisplayTruck([]);
+
+    }, [trucks]);
+
+    const handleDeleteMCP = (id) => {
+        deleteMCP(id);
     };
-    const handleClose = () => {
-        setOpen(false);
-    };
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };
-    const [Item, setAge] = React.useState('');
-    const [selectedValue, setSelectedValue] = React.useState('MCP');
 
-
-
-    //const po=[position.location.lat, position.location.lng];
     return (
         <Container maxWidth={false} sx={{ mx: 0, overflow: 'hidden' }}>
             <Grid container spacing={3} sx={{ my: 2, height: '100%' }}>
-                <Grid item xs={3} columnSpacing={2} sx={{pt:3}}>
+                <Grid item xs={3} columnSpacing={2} sx={{ pt: 3 }}>
                     <Typography textAlign="center" variant="h4">Bản đồ</Typography>
-                    <MapSearch />
+                    <MapSearch 
+                    display={display}
+                    setDisplay={setDisplay}
+                    setDisplayMCP={setDisplayMCP} 
+                    setDisplayTruck={setDisplayTruck} />
+                    <Button onClick={() => setOpen(true)} variant="outlined" fullWidth>Tạo MCP</Button>
+                    <DialogMCP open={open} onClose={() => setOpen(false)} />
+
+                    {/* <Typography>MCP hien thi: {displayMCP.map(x => x._id).join(', ')}</Typography>
+                    <Typography>Truck hien thi: {displayTruck.map(x => x._id).join(', ')}</Typography> */}
+
                 </Grid>
                 <Grid xs={9}>
-                    {position.haveMCPLocation && (
-                        <MapContainer style={{ height: '500px' }} center={[position.location.lat, position.location.lng]} zoom={15} scrollWheelZoom={true}>
-                            <TileLayer
-                                attribution='&copy; <a href="https://www.facebook.com/chacachiene/">Nguyen Phat</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            />
+                    <MapContainer style={{ height: '500px' }} center={[10.876958, 106.794195]} zoom={15} scrollWheelZoom={true}>
+                        <Routing />
 
-                            <Marker icon={mylocation} position={[position.location.lat, position.location.lng]}>
-                                <Popup>
-                                    This is my location.
-                                </Popup>
-                            </Marker>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.facebook.com/chacachiene/">Nguyen Phat</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        {mcps.map(mcp => {
+                            let icon;
+                            if (mcp.load.$numberDecimal / mcp.cap > 0.7) {
+                                icon = MCPfull;
+                            } else if (mcp.load.$numberDecimal / mcp.cap > 0.3) {
+                                icon = MCPhalf;
+                            } else {
+                                icon = MCPemp;
+                            }
+                            return (
+                                <Marker icon={icon} position={[mcp.x.$numberDecimal, mcp.y.$numberDecimal]} key={mcp._id}>
+                                    <Popup>
+                                        Load: {mcp.load.$numberDecimal} <br />
+                                        Capacity: {mcp.cap} <br />
+                                        <ButtonGroup variant="contained" size="xsmall">
+                                            <Button onClick={() => setOpen(true)}><BorderColorIcon /></Button>
+                                            <Button color="error" onClick={() => handleDeleteMCP(mcp._id)}><DeleteIcon /></Button>
+                                        </ButtonGroup>
+                                        <DialogMCP curMCP={mcp} open={open} onClose={() => setOpen(false)} />
+                                    </Popup>
+                                    <Tooltip direction="bottom" style={{ marginTop: '-20px' }}>MCP ID: {mcp._id}</Tooltip>
+                                </Marker>
+                            )
+                        })}
+
+                        {trucks.map(truck => {
+                            return (
+                                <Marker icon={truckIcon} position={[truck.x.$numberDecimal, truck.y.$numberDecimal]} key={truck._id}>
+                                    <Tooltip direction="bottom" style={{ bottom: '0px' }}>Truck ID: {truck._id}</Tooltip>
+                                    <Popup>
+                                        Load: {truck.load.$numberDecimal} <br />
+                                        Capacity: {truck.cap} <br />
+                                        NextMCP: {truck.nextMCP} <br />
+                                        Driver: {truck.driver}
+                                    </Popup>
+                                </Marker>
+                            )
+                        })}
+                        <GetLocation />
 
 
-                            <Marker icon={truckIcon} position={[truckLocation.location.lat, truckLocation.location.lng]}>
-                                <Popup>
-                                    I am a truck <br /> I am going to MCP.
-                                </Popup>
-                            </Marker>
 
-                            <Marker icon={MCPfull} position={[MCPFullLocation.location.lat, MCPFullLocation.location.lng]}>
-                                <Popup>
-                                    This MCP is full <br /> Please come here now.
-                                </Popup>
-                            </Marker>
-
-                            <Marker icon={MCPhalf} position={[MCPHalfLocation.location.lat, MCPHalfLocation.location.lng]}>
-                                <Popup>
-                                    This MCP is half of full <br /> You can come here or not.
-                                </Popup>
-                            </Marker>
-
-                            <Marker icon={MCPemp} position={[MCPEmpLocation.location.lat, MCPEmpLocation.location.lng]}>
-                                <Popup>
-                                    This MCP is empty <br /> Stay away from me.
-                                </Popup>
-                            </Marker>
-                        </MapContainer>
-
-                    )}
+                    </MapContainer>
                 </Grid>
+
             </Grid>
         </Container>
     )
